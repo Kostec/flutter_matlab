5d4d8b8a5d19543345c1b5c0311678b556c4142c
Kostec <50276003+Kostec@users.noreply.github.com>
2020-07-29 18:14:02 +0300

    Редактирование таблицы переменных

--- lib/pages/workspace.dart
+++ lib/pages/workspace.dart
@@ -48,36 +48,49 @@ class _WorkspacePageState extends State<WorkspacePage>{
   }
 
   Widget _buildBody(){
+    _controller = TextEditingController();
+    _controller.addListener(() {
+      final text = _controller.text.toLowerCase();
+      _controller.value = _controller.value.copyWith(
+        text: text,
+        selection:
+        TextSelection(baseOffset: text.length, extentOffset: text.length),
+        composing: TextRange.empty,
+      );
+    });
     List<DataRow> vars = [];
     _workspace.forEach((key, value) {
       vars.add(_buildVariable(key, value));
     });
-    return Container(
-      child: DataTable(
-       columns: [
-         DataColumn(label: Text("Name"), tooltip: 'name'),
-         DataColumn(label: Text("val"), tooltip: 'name'),
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child:
+      Column(
+          children: [
+            DataTable(
+              columns: [
+               DataColumn(label: Text("Name"), tooltip: 'name'),
+               DataColumn(label: Text("Value"), tooltip: 'value'),
+               DataColumn(label: Text("Type"), tooltip: 'type'),
         ],
-          rows: vars,
+        rows: vars,
       )
-//      child: ListView.builder(
-//        itemCount: _workspace.length,
-//        itemBuilder: (context, i){
-//          var key = _workspace.keys.toList()[i];
-//          var value = _workspace[key];
-//          return _buildVariable(key, value);
-//        }
-//      )
+    ])
     );
   }
 
+  TextEditingController _controller;
+
   DataRow _buildVariable(String key, dynamic value){
     print('build var');
+    var valueType = value.runtimeType;
+    TextInputType textType = valueType == int || valueType == double ?  TextInputType.number : TextInputType.text;
     return DataRow(
       cells: [
-        DataCell(Text(key)),
-        DataCell(Text('$value')),
-      ]
+        DataCell(TextFormField(initialValue: '$key', keyboardType: TextInputType.text, onFieldSubmitted: (val){print('onSubmited $key $val');}), showEditIcon: true),
+        DataCell(TextFormField(initialValue: '$value', keyboardType: textType, onFieldSubmitted: (val){print('onSubmited $key $val');},), showEditIcon: true),
+        DataCell(Text('${value.runtimeType.toString()}')),
+      ],
     );
   }
 
