ee5600ef6e3c7780fac7ef37abd712ad77893093
Kostec <50276003+Kostec@users.noreply.github.com>
2020-07-30 18:19:28 +0300

    fix

--- lib/services/library.dart
+++ lib/services/library.dart
@@ -7,14 +7,12 @@ import 'package:fluttermatlab/models/Integrator.dart';
 
 class Library
 {
-  static Map<String, Block> get blocks{
-    return {
+  static Map<String, Block> blocks = {
       'Constant': new Constant(value: 1),
       'Transfer': new TransferFcn(nums: [1], dens: [1, 1]),
       'Integrator': new Integrator(coef: 1),
       'Derivative': new Derivative(),
-    };
-  }
+  };
 
   static Map<String, dynamic> constants = {
     'PI' : 3.14,
