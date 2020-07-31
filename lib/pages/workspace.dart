65233a8d49e1f43f9c4ff2d63ea6f7e8701041dc
Kostec <50276003+Kostec@users.noreply.github.com>
2020-07-31 11:51:43 +0300

    WorkcpacePage доработано переименование переменных

--- lib/pages/workspace.dart
+++ lib/pages/workspace.dart
@@ -14,10 +14,9 @@ class WorkspacePage extends StatefulWidget{
 }
 
 class _WorkspacePageState extends State<WorkspacePage>{
-  Map<String, dynamic> _workspace;
+  Map<String,TextEditingController> _contollers = {};
   @override
   void initState() {
-    _workspace = workspace.variables;
   }
 
   @override
@@ -33,89 +32,77 @@ class _WorkspacePageState extends State<WorkspacePage>{
       ),
     );
   }
-
-  void addVariable(){
-    var n = _workspace.length;
-    workspace.addVariable('name_$n', 0);
-    _workspace = workspace.variables;
-    setState(() { });
-  }
-
   AppBar _buildAppBar(){
     return AppBar(
       title: Text('Workspace'),
     );
   }
-
   Widget _buildBody(){
-    _controller = TextEditingController();
-    _controller.addListener(() {
-      final text = _controller.text.toLowerCase();
-      _controller.value = _controller.value.copyWith(
-        text: text,
-        selection:
-        TextSelection(baseOffset: text.length, extentOffset: text.length),
-        composing: TextRange.empty,
-      );
-    });
+    _contollers = {};
     List<DataRow> vars = [];
-    _workspace.forEach((key, value) {
+    workspace.variables.forEach((key, value) {
       vars.add(_buildVariable(key, value));
     });
     return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child:
-      Column(
-          children: [
-            DataTable(
-              columns: [
-               DataColumn(label: Text("Name"), tooltip: 'name'),
-               DataColumn(label: Text("Value"), tooltip: 'value'),
-               DataColumn(label: Text("Type"), tooltip: 'type'),
-        ],
-        rows: vars,
-      )
-    ])
+        scrollDirection: Axis.vertical,
+        child:
+        Column(
+            children: [
+              DataTable(
+                columns: [
+                  DataColumn(label: Text("Name"), tooltip: 'name'),
+                  DataColumn(label: Text("Value"), tooltip: 'value'),
+                  DataColumn(label: Text("Type"), tooltip: 'type'),
+                ],
+                rows: vars,
+              )
+            ])
     );
   }
-
-  TextEditingController _controller;
-
+  void addVariable(){
+    var n = workspace.variables.length;
+    var name = 'name_$n';
+    while (workspace.variables.containsKey(name)){
+      name = 'name_$n';
+      n++;
+    }
+    workspace.variables[name] = 0;
+    setState(() { });
+  }
   DataRow _buildVariable(String key, dynamic value){
     print('build var');
     var valueType = value.runtimeType;
     TextInputType textType = valueType == int || valueType == double ?  TextInputType.number : TextInputType.text;
+    var controller = new TextEditingController(text: '$key');
+    _contollers['key'] = controller;
     return DataRow(
       cells: [
-        DataCell(TextFormField(initialValue: '$key', keyboardType: TextInputType.text, onFieldSubmitted: (val){ editVariableName(key, val);}), showEditIcon: true),
+//        DataCell(TextFormField(initialValue: '$key', keyboardType: TextInputType.text, onFieldSubmitted: (val){ editVariableName(key, val);}), showEditIcon: true),
+        DataCell(TextFormField(controller: controller, keyboardType: TextInputType.text, onFieldSubmitted: (val){ editVariableName(key, val, controller: controller);}), showEditIcon: true),
         DataCell(TextFormField(initialValue: '$value', keyboardType: textType, onFieldSubmitted: (val){ editVariableValue(key, val);}), showEditIcon: true),
         DataCell(Text('${value.runtimeType.toString()}')),
       ],
     );
   }
-
-  void editVariableName(String key, String value){
-    _workspace.remove(key);
-    _workspace[value] = _workspace[key];
+  void editVariableName(String oldName, String newName, {TextEditingController controller = null}){
+    if (!workspace.variables.containsKey(newName)) {
+      var value = workspace.variables[oldName];
+      workspace.variables.remove(oldName);
+      workspace.variables[newName] = value;
+    }
+    else{
+        controller = new TextEditingController(text: '$workspace.variables[oldName]');
+    }
+    print('');
+    setState((){});
   }
-
   void editVariableValue(String key, String value){
     print('edit $key, $value');
     Type type = workspace.variables[key].runtimeType;
     workspace.variables.remove(key);
     workspace.variables[key] = type == double ? double.parse(value) : value;
   }
-
-  void loadWorkspace(){
-    _workspace = workspace.variables;
-  }
-
-  void saveWorkspace(){
-    workspace.addVariableRange(_workspace);
-  }
-
   void removeWorkspace(){
     workspace.variables.clear();
-    loadWorkspace();
   }
 }
